e59a70e6900536fd459b6c4c4301fd62e56a1d40
Oren Leiman <oleiman@ucsc.edu>
2013-05-23 03:27:37 -0700

    major overhaul to sweet-contracts syntax

--- sweet-contracts.js
+++ sweet-contracts.js
@@ -5,90 +5,80 @@ macro setupContracts {
 }
 
 // by abstracting this out we actually get stuck with *more* parens
-macro opt_obj {
 
-    // case ($($key $[:] ! ($arg:ident) -> {$check ...}) (,) ...) => {
-    // 	{$($key: function ($arg) {$check ...},) ...}
-    // }
-    // case ($($key $[:] ! ($arg:ident) -> {$check ...})) this $[:] $obj => {
-    // 	{$($key: function ($arg) {$check ...},) ..., this: (vbl $obj)}
-    // }
-    case ($($key $[:] $type) (,) ...) => {
-    	{$($key: (vbl $type),) ...}
-    }
-}
-
-macro vbl {
-    case ($p_type -> $ret_type) => {
-	C.fun(vbl $p_type, vbl $ret_type)
-    }
-// case (($p_type -> $ret_type) $[|] $opts) => {
-//     	C.fun([vbl $p_type], vbl $ret_type, opt_obj $opts)
+// macro vbl {
+//     case ($p_type -> $ret_type) => {
+// 	C.fun(vbl $p_type, vbl $ret_type)
 //     }
-    // this is kinda nice, but we're still stuck with parenthesized predicates...
-    case (($p_type -> $ret_type) $[|] $($key $[:] $opt) (,) ...) => {
-    	C.fun([vbl $p_type], vbl $ret_type, {$($key: (vbl $opt),) ...})
-    }
-    // TODO: find an elegant way to allow a method to have 
-    //  pre/post (not both), if possible
-    // case (($p_type -> $ret_type) $[|]
-    //          pre:  $pre_cond
-    //          post: $post_cond) => {
-    // 	C.fun([vbl $p_type], vbl $ret_type, {
-    // 	    pre: vbl $pre_cond,
-    // 	    post: vbl $post_cond})
-    // }
-    // case (($p_type -> $ret_type) $[|]
-    //          pre:  $pre_cond) => {
-    // 	C.fun([vbl $p_type], vbl $ret_type, {
-    // 	    pre: vbl $pre_cond})
-    // }
-    // case (($p_type -> $ret_type) $[|]
-    //          post: $post_cond) => {
-    // 	C.fun([vbl $p_type], vbl $ret_type, {
-    // 	    post: vbl $post_cond})
-    // }
-    case ($($key $[:] $type) (,) ...) => {
-	C.object({$($key: (vbl $type),) ...})
-    }
-    case (($($key $[:] $type) (,) ...) $[|] $pred) => {
-	C.object({$($key: (vbl $type),) ...}, 
-		 {invariant: vbl $pred})
-    }
-    case [$type (,) ...] => {
-	C.arr([(vbl $type) (,) ...])
-    }
-    case (? $comb) => {
-	C.opt(C.$comb)
-    }
-    case ($comb1 or $comb2) => {
-	C.or(vbl $comb1, vbl $comb2)
-    }
-    case ($comb1 and $comb2) => {
-	C.and(vbl $comb1, vbl $comb2)
-    }
-    case (! ($arg:ident) -> { $check ... }) => {
-	function ($arg) {
-	    $check ...
-	}
-    }
-    case (! ($args:ident, $result:ident) -> { $check ... }) => {
-	function ($args) { 
-    	    return C.check(
-		function ($result) { $check ... }, 'foobar')
-	}
-    }
-    case $comb => {
-	C.$comb 
-    }
-    case [$type $[...]] => {
-    	C.arr([C.___(vbl $type)])
-    }
-}
+// // case (($p_type -> $ret_type) $[|] $opts) => {
+// //     	C.fun([vbl $p_type], vbl $ret_type, opt_obj $opts)
+// //     }
+//     // this is kinda nice, but we're still stuck with parenthesized predicates...
+//     case (($p_type -> $ret_type) $[|] $($key $[:] $opt) (,) ...) => {
+//     	C.fun([vbl $p_type], vbl $ret_type, {$($key: (vbl $opt),) ...})
+//     }
+//     // TODO: find an elegant way to allow a method to have 
+//     //  pre/post (not both), if possible
+//     // case (($p_type -> $ret_type) $[|]
+//     //          pre:  $pre_cond
+//     //          post: $post_cond) => {
+//     // 	C.fun([vbl $p_type], vbl $ret_type, {
+//     // 	    pre: vbl $pre_cond,
+//     // 	    post: vbl $post_cond})
+//     // }
+//     // case (($p_type -> $ret_type) $[|]
+//     //          pre:  $pre_cond) => {
+//     // 	C.fun([vbl $p_type], vbl $ret_type, {
+//     // 	    pre: vbl $pre_cond})
+//     // }
+//     // case (($p_type -> $ret_type) $[|]
+//     //          post: $post_cond) => {
+//     // 	C.fun([vbl $p_type], vbl $ret_type, {
+//     // 	    post: vbl $post_cond})
+//     // }
+//     case ($($key $[:] $type) (,) ...) => {
+// 	C.object({$($key: (vbl $type),) ...})
+//     }
+//     case (($($key $[:] $type) (,) ...) $[|] $pred) => {
+// 	C.object({$($key: (vbl $type),) ...}, 
+// 		 {invariant: vbl $pred})
+//     }
+//     case [$type (,) ...] => {
+// 	C.arr([(vbl $type) (,) ...])
+//     }
+//     case (? $comb) => {
+// 	C.opt(C.$comb)   
+//     }
+//     case ($comb1 or $comb2) => {
+// 	C.or(vbl $comb1, vbl $comb2)
+//     }
+//     case ($comb1 and $comb2) => {
+// 	C.and(vbl $comb1, vbl $comb2)
+//     }
+//     case (! ($arg:ident) -> { $check ... }) => {
+// 	function ($arg) {
+// 	    $check ...
+// 	}
+//     }
+//     case (! ($args:ident, $result:ident) -> { $check ... }) => {
+// 	function ($args) { 
+//     	    return C.check(
+// 		function ($result) { $check ... }, 'foobar')
+// 	}
+//     }
+//     case $comb => {
+// 	C.$comb 
+//     }
+//     case [$type $[...]] => {
+//     	C.arr([C.___(vbl $type)])
+//     }
+// }
 
 // TODO: we're stuck with 'some arbitrary name' here. This will be
 //       fixed when sweet.js gets some new features
 
+
+// TODO: this needs to change. see notes
 macro bang {
     case ($param:ident) -> $body:expr => {
 	C.check(function ($param) $body, 'some arbitrary name');
@@ -106,41 +96,168 @@ macro def {
     }
 }
 
-macro fun {
-    case ($type (,) ...) -> $ret_type var $handle:ident = function $args $body => {
-	var $handle = C.guard(
-	    C.fun([(vbl $type) (,) ...], vbl $ret_type),
-	    function $args $body);
+
+macro vbl {
+    case ($[?] $comb1, $rest ...) => {
+	vbl (? $comb1), vbl ($rest ...)
     }
-    case ($type (,) ...) -> $ret_type function $handle $args $body => {
-	var $handle = C.guard(
-	    C.fun([(vbl $type) (,) ...], vbl $ret_type),
-	    function $args $body);
+    case ($p_type -> $ret_type $[|] $rest ...) => {
+	C.fun(vbl ($p_type), vbl ($ret_type), {
+	    vbl ($rest ...)
+	})
     }
-    // ctor
-    case ($type (,) ...) ==> $ret_type var $handle:ident = function $args $body => {
-	var $handle = C.guard(
-	    C.fun([(vbl $type) (,) ...], vbl $ret_type, {newOnly: true}),
-	    function $args $body);
+    case ($key:ident $[:] ! ($arg) -> { $check ... }, $rest ...) => {
+	$key: function($arg) { $check ... }, vbl ($rest ...)
     }
-    case ($type (,) ...) ==> $ret_type function $handle $args $body => {
-	var $handle = C.guard(
-	    C.fun([(vbl $type) (,) ...], vbl $ret_type, {newOnly: true}),
-	    function $args $body);
+    case ($key:ident $[:] ! ($arg) -> { $check ... }) => {
+	$key: function($arg) { $check ... }
     }
-    // no ctor
-    case ($type (,) ...) --> $ret_type var $handle:ident = function $args $body => {
-	var $handle = C.guard(
-	    C.fun([(vbl $type) (,) ...], vbl $ret_type, {callOnly: true}),
-	    function $args $body);
+    // the 'this' contract (for objects) goes here
+    case ($p_type -> $ret_type, $rest ...) => {
+    	vbl ($p_type -> $ret_type), vbl ($rest ...)
     }
-    case ($type (,) ...) --> $ret_type function $handle $args $body => {
-	var $handle = C.guard(
-	    C.fun([(vbl $type) (,) ...], vbl $ret_type, {callOnly: true}),
-	    function $args $body);
+    case {$key $[:] $rest ...} => {
+	C.object({$key: vbl ($rest ...)})
+    }
+    case ($key $[:] $rest ...) => {
+	$key: vbl ($rest ...)
+    }
+    case ([$comb, $arr ...], $rest ...) => {
+    	C.arr([vbl ($comb), vbl ($arr ...)]), vbl ($rest ...)
+    }
+    // this contract must come at the end of param list
+    case ($[#] $this) => {
+	vbl ($this)
+    }
+    // can't figure our how to get arbitrarily many and/or withouut parens...
+    case ($comb1 or $comb2, $rest ...) => {
+    	C.or(vbl ($comb1), vbl ($comb2)), vbl ($rest ...)
+    }
+    case ($comb1 and $comb2, $rest ...) => {
+    	C.and(vbl ($comb1), vbl ($comb2)), vbl ($rest ...)
+    }
+    case ($comb:ident, $rest ...) => {
+	C.$comb, vbl ($rest ...)
+    }
+    // base cases
+    case $p_type -> $ret_type $[|] $opts => {
+    	C.fun ([vbl $p_type], vbl $ret_type, {
+    	    opts $opts
+    	})
+    }
+    case ($p_type -> $ret_type) => {
+	C.fun ([vbl $p_type], vbl $ret_type)
+    }
+    case (! ($args:ident, $result:ident) -> { $check ... }) => {
+	function ($args) {
+	    return C.check(
+		function ($result) { $check ... }, 'dependent')
+	}
+    }
+    case [$comb, $rest ...] => {
+	C.arr([vbl ($comb), vbl ($rest ...)])
+    }
+    case ($comb1 and $comb2) => {
+	c.and(vbl ($comb1), vbl ($comb2))
+    }
+    case ($comb1 or $comb2) => {
+	C.or(vbl ($comb1), vbl ($comb2))
+    }
+    case ($key: ! ($arg) -> { $check ...} ) => {
+	function ($arg) {
+	    $check ...
+	}
+    }
+    // this is colliding with the regular ellipses
+    case ($comb $[...]) => {
+	C.___(vbl ($comb))
+    }
+    case [$comb] => {
+    	C.arr([vbl ($comb)])
+    }
+    case [$comb $[...]] => {
+    	C.arr([vbl ($comb)])
+    }
+    case ($[?] $comb) => {
+	C.opt(vbl ($comb))
+    }
+    case ($comb) => {
+    	vbl $comb
+    }
+    case $comb:ident => {
+	C.$comb
     }
 }
 
+// so many cases...makes the syntax pretty, though
+macro fun {
+    case ($parms ...) -> ($ret ...) function $handle $args $body => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl ($ret ...)),
+    	    function $args $body);
+    }
+    case ($parms ...) -> ! ($argl:ident, $result:ident) -> {$check ...} function $handle $args $body => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl (! ($argl, $result) -> {$check ...})),
+    	    function $args $body);
+    }
+    case ($parms ...) -> ($ret ...) var $handle = $def:expr => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl ($ret ...)),
+    	    $def);
+    }
+    case ($parms ...) -> ! ($argl:ident, $result:ident) -> {$check ...} var $handle = $def:expr => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl (! ($argl, $result) -> {$check ...})),
+    	    $def);
+    }
+
+    // no new
+    case ($parms ...) --> ($ret ...) function $handle $args $body => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl ($ret ...), {callOnly: true}),
+    	    function $args $body);
+    }
+    case ($parms ...) --> ! ($argl:ident, $result:ident) -> {$check ...} function $handle $args $body => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl (! ($argl, $result) -> {$check ...}), {callOnly: true}),
+    	    function $args $body);
+    }
+    case ($parms ...) --> ($ret ...) var $handle = $def:expr => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl ($ret ...), {callOnly: true}),
+    	    $def);
+    }
+    case ($parms ...) --> ! ($argl:ident, $result:ident) -> {$check ...} var $handle = $def:expr => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl (! ($argl, $result) -> {$check ...}), {callOnly: true}),
+    	    $def);
+    }
+
+    // only new
+    case ($parms ...) ==> ($ret ...) function $handle $args $body => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl ($ret ...), {newOnly: true}),
+    	    function $args $body);
+    }
+    case ($parms ...) ==> ! ($argl:ident, $result:ident) -> {$check ...} function $handle $args $body => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl (! ($argl, $result) -> {$check ...}), {newOnly: true}),
+    	    function $args $body);
+    }
+    case ($parms ...) ==> ($ret ...) var $handle = $def:expr => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl ($ret ...), {newOnly: true}),
+    	    $def);
+    }
+    case ($parms ...) ==> ! ($argl:ident, $result:ident) -> {$check ...} var $handle = $def:expr => {
+    	var $handle = C.guard(
+    	    C.fun([vbl ($parms ...)], vbl (! ($argl, $result) -> {$check ...}), {newOnly: true}),
+    	    $def);
+    }
+}
+
+// double duty objects and arrays
 macro obj {
     case $contract var $handle:ident = $obj => {
 	var $handle = C.guard(vbl $contract, $obj)
@@ -150,14 +267,47 @@ macro obj {
 var contracts = window['contracts-js'];
 setupContracts(contracts)
 
+vbl [Num, Str]
 
-obj (a: Num, 
-     b: ((Num -> Num) |
-          pre: (!(o) -> { return o.a > 10; }),
-          post: (!(o) -> { return o.a > 20; }),
-	  this: (a: Num, b: (Num->Num))))
-var ppo = {a: 3, b: function (x) {return this.a = this.a + x;} };
-ppo.b(3);
+vbl (Str or Str, ? Num, (Bool, Num) -> (Str or Bool), [Num, Str], Num)
+
+fun (Num, {x: (Num) -> Str | 
+    pre: !(x) -> {return x.a > 10;}, 
+    post: !(x) -> {return x.a > 20}
+     }) ==> (Str or Num)
+function baz (x, y) { return y; }
+
+fun (Num, #{name: Str}) -> (Str)
+function batch (x) { return this.name; }
+
+obj {x: (Num) -> Str | 
+       pre: !(x) -> {return x.a > 10;}, 
+       post: !(x) -> {return x.a > 20} }
+var see = {x: function(y) {return y > 3;}};
+
+// fun (Num, Str or Bool, (Str)->(Str or Num)) -> (Num)
+// function baz (x, y, z) { return x; }
+
+// vbl {x: Num, y: (Str) -> ! (args, result) -> { return args[0]} }
+
+fun (Num or Bool, Str) -> ! (args, result) -> { return args[0] > result; } 
+function foo (x, y) { return x - 1; }
+
+// var q = opts (pre: ! (x) -> { return x.a > 10; })
+
+
+// fun (Num or Str, Str or Bool) -> (Str and Str or Num)
+// function bar(x, y) {
+//     return y + x;
+// }
+
+// obj (a: Num, 
+//      b: ((Num -> Num) |
+//           pre: (!(o) -> { return o.a > 10; }),
+//           post: (!(o) -> { return o.a > 20; }),
+// 	  this: (a: Num, b: (Num->Num))))
+// var ppo = {a: 3, b: function (x) {return this.a = this.a + x;} };
+// ppo.b(3);
 
 // obj (a: Num, 
 //      b: ((Num -> Num) |
