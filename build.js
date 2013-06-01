4f7ca5f49113d3baf6ff30cb5e10d4ef0e181565
Oren Leiman <oleiman@ucsc.edu>
2013-06-01 02:15:40 -0700

    updates build file to reflect npm package infrastructure

--- build.js
+++ build.js
@@ -1,6 +1,3 @@
-// TODO: provide some facility to build only a single test. Or perhaps just
-//       include build/*.js in the repository, cut out the node middleman...
-
 require("shelljs/make");
 var path = require("path");
 var fs = require("fs");
@@ -25,34 +22,31 @@ target.build = function() {
     }
 };
 
-// TODO: this is tightly coupled to the structure of my local code directory...
-//       perhaps allow user to pass in the location of the sjs binary?
-//       this is really a problem with the node module...should include -m option
 target.build_test = function() {
     ls("test/*.js").forEach(function(file) {
         echo("compiling: " + path.basename(file));
-        exec("../sweet.js/bin/sjs -o build/" + path.basename(file) + " -m sweet-contracts.js " + file);
+        exec("bin/sweet-contracts -o build/" + path.basename(file) + " " + file);
     });
 };
 
 target.test_functions = function() {
     target.clean();
     echo("compiling: test_function_contracts");
-    exec("../sweet.js/bin/sjs -o build/test_function_contracts.js -m sweet-contracts.js ./test/test_function_contracts.js");
+    exec("bin/sweet-contracts -o ./build/test_function_contracts.js ./test/test_function_contracts.js");
     target.test();
 };
 
 target.test_objects = function() {
     target.clean();
     echo("compiling: test_object_contracts");
-    exec("../sweet.js/bin/sjs -o build/test_object_contracts.js -m sweet-contracts.js ./test/test_object_contracts.js");
+    exec("bin/sweet-contracts -o ./build/test_object_contracts.js ./test/test_object_contracts.js");
     target.test();
 };
 
 target.test_advanced = function() {
     target.clean();
     echo("compiling: test_advanced_contracts");
-    exec("../sweet.js/bin/sjs -o build/test_advanced_contracts.js -m sweet-contracts.js ./test/test_advanced_contracts.js");
+    exec("bin/sweet-contracts -o ./build/test_advanced_contracts.js ./test/test_advanced_contracts.js");
     target.test();
 };
 
